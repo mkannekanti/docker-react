e31dd35bd45ca6eed2740f9a80c06506d28e8e4e
Mohan Kumar Kannekanti <mohan.kannekanti@gmail.com>
2019-07-31 13:37:33 -0700

    Adding feature branch message

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hurray 2!!!
+          Saying hi from feature branch
         </p>
       </div>
     );
